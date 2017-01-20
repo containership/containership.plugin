389093da87f43bec51faab3a43d8ac03105dc0f4
Jeremy Kross <jeremy.kross@containership.io>
2017-01-20 19:25:38 +0000

    Initial Commit.

---
+++ index.js
@@ -0,0 +1,9 @@
+const ContainershipPlugin = require('./lib/containership-plugin');
+const HostImplementation = require('./lib/host-implementation');
+const ApiImplementation = require('./lib/api-implementation');
+
+module.exports = {
+    ContainershipPlugin,
+    HostImplementation,
+    ApiImplementation
+};
