bb41b09faab8bf3ea29a3c3ca0dbfa773ca1b76a
Jeremy Kross <jeremy.kross@containership.io>
2017-05-01 15:11:32 +0000

    5/2017 checkpoint.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const ContainershipPlugin = require('./lib/containership-plugin');
 const HostImplementation = require('./lib/host-implementation');
 const ApiImplementation = require('./lib/api-implementation');
+const ApiBuilder = require('./lib/api-builder');
 
 module.exports = {
     ContainershipPlugin,
     HostImplementation,
-    ApiImplementation
+    ApiImplementation,
+    ApiBuilder
 };
