a1923258eecde1fd33ac0ee8a8268f41c04b6c49
Jeremy Kross <jeremy.kross@containership.io>
2017-05-23 18:45:30 +0000

    Refactored some code into separate projects, added CS orchestrator shim.

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const ContainershipPlugin = require('./lib/containership-plugin');
-const HostImplementation = require('./lib/host-implementation');
-const ApiImplementation = require('./lib/api-implementation');
 const ApiBuilder = require('./lib/api-builder');
 
 module.exports = {
     ContainershipPlugin,
-    HostImplementation,
-    ApiImplementation,
     ApiBuilder
 };
